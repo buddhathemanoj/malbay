3bae295d95953fed0357dcac50d3eeb5057b1f9b
AshokKumar185 <ashokkumar936127@gmail.com>
2023-10-17 17:23:12 +0530

    create locations

--- src/components/Locations/location.js
+++ src/components/Locations/location.js
@@ -1,12 +1,51 @@
 import "./location.css"
-
+import sekinchan from "../../asset/sekinchan.jpg"
+import mersing from "../../asset/mersing-1654740355.jpg"
+import lumbur from "../../asset/kuala-lumpur-1654740535.jpg"
+import highlands from "../../asset/cameron-highland-1654740575.jpg"
 const Location = () => {
     return(
         <div className="loacation-bg-container">
             <div className="sekinchan-container">
-                <h1>Sekinchan: Abundant Farming Haven</h1>
-                <p>The name "Sekinchan" in Chinese translates to 'village ideal for cultivation'. The fertile land and favorable climate are perfect for cultivating rice, fruits, and oil palm trees. This area boasts the highest rice production per acre in Malaysia, making it a prime location for agriculture.
+            <div className="framing-container">
+                <h1 className="framing-heading">SEKINCHAN: Abundant Farming Haven</h1>
+                  <p className="framing-desc">The name "Sekinchan" in Chinese translates to 'village ideal for cultivation'. The fertile land and favorable climate are perfect for cultivating rice, fruits, and oil palm trees. This area boasts the highest rice production per acre in Malaysia, making it a prime location for agriculture.
                     The total farming land dedicated to paddy fields and fruits spans approximately 4,300 acres.</p>
+                    <button className="book-now-btn">BOOK NOW</button>
+                </div>
+                <img src={sekinchan} alt="location" width={600} style={{borderRadius:"5px"}} />
+            </div>
+
+            <div className="sekinchan-container">
+            <img src={mersing} alt="location" width={600} style={{borderRadius:"5px"}} />
+            <div className="framing-container">
+                <h1 className="framing-heading">Mersing: Gateway to Natural Beauty and Heritage</h1>
+                  <p className="framing-desc">Situated on the east coast of the Malaysian state of Johor, Mersing town beckons as a portal to nature and rich cultural heritage. Its fame stems from the bustling passenger boat jetty,
+                   a launching point for daily boat excursions to an array of picturesque offshore islands.</p>
+                    <button className="book-now-btn">BOOK NOW</button>
+                </div>
+                
+            </div>
+
+            <div className="sekinchan-container">
+            <div className="framing-container">
+                <h1 className="framing-heading">Kuala Lumpur: Urban Marvel with Islamic Architecture</h1>
+                  <p className="framing-desc">Kuala Lumpur, the vibrant capital of Malaysia, boasts a modern skyline dominated by the majestic Petronas Twin Towers. These iconic skyscrapers, standing tall at 451 meters, are adorned with elegant glass-and-steel architecture infused with intricate Islamic motifs. 
+                  Visitors can explore the towers via a public skybridge and an observation deck, immersing themselves in the awe-inspiring cityscape.</p>
+                    <button className="book-now-btn">BOOK NOW</button>
+                </div>
+                <img src={lumbur} alt="location" width={600} style={{borderRadius:"5px"}} />
+            </div>
+
+            <div className="sekinchan-container">
+            <img src={highlands} alt="location" width={600} style={{borderRadius:"5px"}} />
+            <div className="framing-container">
+                <h1 className="framing-heading">Cameron Highlands: A Haven for Nature Enthusiasts</h1>
+                  <p className="framing-desc">Cameron Highlands is a haven for nature enthusiasts seeking adventure and scenic pathways for exploration.
+                   Take a leisurely stroll and uncover special spots like the enchanting Mossy Forest, making it an ideal destination for those who appreciate the beauty of the natural world.</p>
+                    <button className="book-now-btn">BOOK NOW</button>
+                </div>
+                
             </div>
         </div>
     )
