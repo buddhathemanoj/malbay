9b92ba967c7c41fc25d95f02c2466093d4160dae
AshokKumar185 <ashokkumar936127@gmail.com>
2023-10-18 15:21:44 +0530

    location

--- src/components/Locations/location.js
+++ src/components/Locations/location.js
@@ -13,11 +13,11 @@ const Location = () => {
                     The total farming land dedicated to paddy fields and fruits spans approximately 4,300 acres.</p>
                     <button className="book-now-btn">BOOK NOW</button>
                 </div>
-                <img src={sekinchan} alt="location" width={600} style={{borderRadius:"5px"}} />
+                <img src={sekinchan} alt="location" width={600} style={{borderRadius:"5px"}} className="location-img" />
             </div>
 
-            <div className="sekinchan-container">
-            <img src={mersing} alt="location" width={600} style={{borderRadius:"5px"}} />
+            <div className="mersing-container">
+            <img src={mersing} alt="location" width={600} style={{borderRadius:"5px"}} className="location-img" />
             <div className="framing-container">
                 <h1 className="framing-heading">Mersing: Gateway to Natural Beauty and Heritage</h1>
                   <p className="framing-desc">Situated on the east coast of the Malaysian state of Johor, Mersing town beckons as a portal to nature and rich cultural heritage. Its fame stems from the bustling passenger boat jetty,
@@ -27,18 +27,18 @@ const Location = () => {
                 
             </div>
 
-            <div className="sekinchan-container">
+            <div className="lumbur-container">
             <div className="framing-container">
                 <h1 className="framing-heading">Kuala Lumpur: Urban Marvel with Islamic Architecture</h1>
                   <p className="framing-desc">Kuala Lumpur, the vibrant capital of Malaysia, boasts a modern skyline dominated by the majestic Petronas Twin Towers. These iconic skyscrapers, standing tall at 451 meters, are adorned with elegant glass-and-steel architecture infused with intricate Islamic motifs. 
                   Visitors can explore the towers via a public skybridge and an observation deck, immersing themselves in the awe-inspiring cityscape.</p>
                     <button className="book-now-btn">BOOK NOW</button>
                 </div>
-                <img src={lumbur} alt="location" width={600} style={{borderRadius:"5px"}} />
+                <img src={lumbur} alt="location" width={600} style={{borderRadius:"5px"}} className="location-img" />
             </div>
 
-            <div className="sekinchan-container">
-            <img src={highlands} alt="location" width={600} style={{borderRadius:"5px"}} />
+            <div className="highlands-container">
+            <img src={highlands} alt="location" width={600} style={{borderRadius:"5px"}} className="location-img" />
             <div className="framing-container">
                 <h1 className="framing-heading">Cameron Highlands: A Haven for Nature Enthusiasts</h1>
                   <p className="framing-desc">Cameron Highlands is a haven for nature enthusiasts seeking adventure and scenic pathways for exploration.
