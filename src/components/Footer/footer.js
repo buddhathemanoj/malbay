018bde2e0ab7424b3ae923fe0e10e079ae980d01
buddhathemanoj <prabhakarmanoj743@gmail.com>
2023-10-31 16:38:21 +0530

    footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -12,22 +12,23 @@ const Footer = () => {
                 <div className="row">
                     <div className="col-12 col-md-4 mb-3 para">
                         <h3 className='logoname'><Link to='/'>Malbay Express</Link></h3>
-                        <p>Malbay Express offers dependable, secure, and cost-effective MVP Private Car journeys from Singapore to Johor Bahru, Legoland Malaysia, and various other destinations within Malaysia.</p>  
+                        <p>At Malbay Express, we provide reliable, safe, and budget-friendly MVP Private Car services for travelers heading from Singapore to Johor Bahru, Legoland Malaysia, and numerous other destinations across Malaysia. Your trusted partner for seamless and cost-effective journeys.</p>  
                     </div>
                     <div className="col-12 col-md-4 mb-3">
                         <ul className="list-unstyled">
-                            <li className='linksfooter'><Link to="/">Home</Link></li>
+                            <li className='linksfooter'><Link to="/">HOME</Link></li>
                             <li><Link to="/about">ABOUT</Link></li>
                             <li><Link to="/services">SERVICES</Link></li>
                             <li><Link to="/locations">LOCATIONS</Link></li>
                             <li><Link to="/fleets">FLEETS</Link></li>
                             <li><Link to="/pricing">PRICING</Link></li>
-                            <li><Link to="/contact">contact</Link></li>
+                            <li><Link to="/contact">CONTACT</Link></li>
                         </ul>
                     </div>
                     <div className="col-12 col-md-4 mb-3">
                         <p><FontAwesomeIcon icon={faPhone} className="me-2" />+(91)6369487527</p>
-                        <p><FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />22, teachers colony, Tirunelveli<br/>TamilNadu</p>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />316C Anchorvale Link 05 - 209 <br/>Anchorvale Gardens<br/>Singapore<br/>543316</p>
+                        <p><FontAwesomeIcon icon={faEnvelope} className="me-2" />flynewtours@gmail.com</p>
                     </div>
                 </div>
             </div>
